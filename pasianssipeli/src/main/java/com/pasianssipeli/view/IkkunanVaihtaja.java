79e6342dcc0901a5d46e369cf1e8217c494f0eb4
Daxu223 <samu.kollin@gmail.com>
2024-04-12 14:40:03 +0300

    Vaihdettu pelipaneelin nimi

--- pasianssipeli/src/main/java/com/pasianssipeli/view/IkkunanVaihtaja.java
+++ pasianssipeli/src/main/java/com/pasianssipeli/view/IkkunanVaihtaja.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 // Creates the main window, which is the basis for all the windows.
-public class PaaIkkuna {
+public class IkkunanVaihtaja {
     public static final int WINDOW_WIDTH = 1080, WINDOW_HEIGHT = 720;
 
     // Use static, so multiple instances of window are not created.
