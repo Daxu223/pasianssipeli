f212dab8978158f5a9d7d5bc8182207fc5ae5e1d
Daxu223 <samu.kollin@gmail.com>
2024-04-15 15:33:43 +0300

    sdjfasjzidgfaewrf

--- pasianssipeli/src/main/java/com/pasianssipeli/controller/Peliohjain.java
+++ pasianssipeli/src/main/java/com/pasianssipeli/controller/Peliohjain.java
@@ -1,5 +1,10 @@
 package com.pasianssipeli.controller;
 
 public class Peliohjain {
-    
+    /*
+     * 1. Korttien siirto, lähetä käsky kortin sijainnille, päivitä korttia viewissä
+     * 2. Alusta kortit, kutsu pelitilaa ja anna sille käsky tehdä jotain
+     * 3. Luo tarvittavat oliot pakan ja muiden tekemiseen.
+     * 4. 
+     */
 }
