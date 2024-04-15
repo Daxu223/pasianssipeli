a993deeaa5a238a58052541af784db807dca8d7d
Daxu223 <samu.kollin@gmail.com>
2024-04-15 15:32:50 +0300

    Lisätty kommentteja seuraaville vaiheille

--- pasianssipeli/src/main/java/com/pasianssipeli/controller/HiirenKuuntelija.java
+++ pasianssipeli/src/main/java/com/pasianssipeli/controller/HiirenKuuntelija.java
@@ -1,5 +1,9 @@
 package com.pasianssipeli.controller;
 
 public class HiirenKuuntelija {
-    
+    /* Hiiren kuuntelija kuuntelee hiirtä
+     * Jos käyttäjä painaa hiirtä kortin yläpuolella,
+     * niin odota kunnes käyttäjä päästää irti
+     * ja kysy model/kortti.java:lta mitä tapahtuu
+     */
 }
