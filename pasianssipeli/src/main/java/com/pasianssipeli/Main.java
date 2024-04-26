df8b758451cec8778b9d846e3340a5138c4e06e4
Daxu223 <samu.kollin@gmail.com>
2024-04-26 15:15:18 +0300

    Vissiin räjähtää

--- pasianssipeli/src/main/java/com/pasianssipeli/Main.java
+++ pasianssipeli/src/main/java/com/pasianssipeli/Main.java
@@ -3,7 +3,6 @@ package com.pasianssipeli;
 import javax.swing.*;
 
 import com.pasianssipeli.view.MainFrame;
-import com.pasianssipeli.view.TestFrame;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
