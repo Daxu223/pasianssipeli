18abdf4b60f7c102cdca9db531bf2e5a153cc00b
Daxu223 <samu.kollin@gmail.com>
2024-05-02 14:24:17 +0300

    Leikitty layouttien kanssa. Päädyin GridBagLayouttiin. Yläpaneelin view valmis. Siirryn alapalkkiin

--- pasianssipeli/src/main/java/com/pasianssipeli/view/PeliPaneeli.java
+++ pasianssipeli/src/main/java/com/pasianssipeli/view/PeliPaneeli.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import javax.swing.*;
 
 public class PeliPaneeli extends JPanel {
-    private static final int PINOJEN_MAARA = 2; // Yksi pakalle, josta nostetaan ja toinen toinen poistopakalle
     private static final int FOUNDATION_PAKKOJEN_MAARA = 4; // Tähän laitetaan kortit, joita etsitään.
     private final Dimension korttikoko = new Dimension(96, 128);
     
@@ -21,50 +20,68 @@ public class PeliPaneeli extends JPanel {
         
     }
 
-    // Yläpaneeli, pakka ja talletettavat kortit
+    // Yläpaneeli, pakka ja talletettavat kortit. Käytetään GridBagLayouttia pohjana
     private JPanel makeTopPanel() {
        
-        JPanel topPanel = new JPanel(); // Create top columns
-        topPanel.setOpaque(false);
-        FlowLayout flowLayout = new FlowLayout(FlowLayout.LEFT, 10, 5);
-        flowLayout.setAlignOnBaseline(true);
-        topPanel.setLayout(flowLayout);
-        
-        
+        JPanel topPanel = new JPanel();
+        GridBagLayout layout = new GridBagLayout(); // 3 riviä, 2 saraketta, 10 pikselin väliä
+        topPanel.setLayout(layout);
+        GridBagConstraints c = new GridBagConstraints();
+
+        // Asetetaan oletusasetuksia GridBagConstraints-olioon
+        c.fill = GridBagConstraints.HORIZONTAL; // Komponentin täyttö suunta
+        c.insets = new Insets(10, 10, 10, 10);  // Välit jokaisen komponentin ympärillä
+        c.gridy = 0; // kaikki samalla rivillä
+
         // Lisää nostopakka
-        ImageIcon pakkaIcon = new ImageIcon(getClass().getResource("../../../cards/back001.gif"));
-        JLabel pakka = new JLabel("Nosto pakka", pakkaIcon, JLabel.CENTER);
-        pakka.setHorizontalTextPosition(JLabel.CENTER);
-        pakka.setVerticalTextPosition(JLabel.BOTTOM);
-        pakka.setPreferredSize(korttikoko);
-        pakka.setBounds(100, 100, 100, 100);
-        topPanel.add(pakka);
+        ImageIcon kortinTaka = new ImageIcon(getClass().getResource("../../../cards/back001.gif"));
+        JLabel pakka = new JLabel(kortinTaka);
+        c.gridx = 0; // Ensimmäinen sarake
+        c.weightx = 0.25; // Kuinka paljon komponentti saa suhteessa muihin komponentteihin vaakasuunnassa
+        topPanel.add(pakka, c);
 
         // Poisto pakka: eli kortit johon kortit nostetaan
         JLayeredPane poistopakka = new JLayeredPane();
-        poistopakka.setPreferredSize(new Dimension(120, 128)); // Adjusted for label
+        poistopakka.setPreferredSize(new Dimension(120, 128)); // Asetetaan sopiva koko
 
+        // Luo ja aseta kortit poistopakkaan
         for (int i = 0; i < 3; i++) {
-            JLabel kortti = new JLabel(pakkaIcon); // Tähän tulisi kortteja randomilla
-            int yOffset = korttikoko.height - 138; // Säädä layeredPane ja flowLayout samalle tasolle.
-            kortti.setBounds(10 * i, yOffset, korttikoko.width, korttikoko.height);
-            poistopakka.add(kortti);
+            JLabel kortti = new JLabel(kortinTaka);
+            kortti.setBounds(10 * i, 0, korttikoko.width, korttikoko.height); // Kortit kerroksittain
+            poistopakka.add(kortti, Integer.valueOf(i)); // Kerros määritetään Integer-arvolla
         }
-
-        topPanel.add(poistopakka);
+  
+        c.gridx = 1; // Sijoita poistopakka toiseen sarakkeeseen
+        c.weightx = 0.25; // Jakaa lisätilan tämän solun ja muiden solujen välillä
+        topPanel.add(poistopakka, c);
 
         // Tällä säädetään pinojen ja foundation pinojen etäisyyttä
-        topPanel.add(Box.createHorizontalStrut(350));
+        c.gridx = 2;
+        c.weightx = 0.5; // antaa enemmän tilaa keskelle
+        topPanel.add(Box.createHorizontalStrut(300), c);
 
-        for (int number = 1; number < FOUNDATION_PAKKOJEN_MAARA + 1; number++) {
-            ImageIcon foundationIcon = new ImageIcon(getClass().getResource("../../../cards/fpBase0"+ number + ".gif"));
-            JLabel kohdepakka = new JLabel("Foundation " + number, foundationIcon, JLabel.CENTER);
-            kohdepakka.setPreferredSize(korttikoko); // Match dimensions
-            kohdepakka.setHorizontalTextPosition(JLabel.CENTER);
-            kohdepakka.setVerticalTextPosition(JLabel.BOTTOM);
-            kohdepakka.setBounds(10 * number, 0, korttikoko.width - 10, korttikoko.height);
-            topPanel.add(kohdepakka);
+
+        for (int i = 1; i < FOUNDATION_PAKKOJEN_MAARA + 1; i++) {
+            ImageIcon foundationIcon = new ImageIcon(getClass().getResource("../../../cards/fpBase0"+ i + ".gif"));
+            JLabel perusta = new JLabel(foundationIcon);
+            perusta.setPreferredSize(korttikoko);
+            perusta.setBounds(i * 10, 0, korttikoko.width, korttikoko.height);
+            c.gridx = 2 + i;
+            c.weightx = 1;
+            topPanel.add(perusta, c);
         }
+
+        // Tee separator, niin yläpaneeli ja muut osiot voidaan erottaa
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = GridBagConstraints.REMAINDER; // Asettaa erottajan kattamaan kaikki sarakkeet
+        c.weightx = 1; // Kaikki jäljellä oleva tila
+        c.fill = GridBagConstraints.HORIZONTAL;
+        JSeparator separator = new JSeparator();
+        separator.setPreferredSize(new Dimension(1, 1));
+        separator.setOrientation(SwingConstants.HORIZONTAL);
+        topPanel.add(separator, c);
+
         return topPanel;
 
     }
