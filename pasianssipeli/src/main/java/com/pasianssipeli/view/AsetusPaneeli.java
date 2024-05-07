a18fcff3e19a7c3614b4ab437316647cdb82dee5
Samu Kollin <samu.kollin@student.oulu.fi>
2024-05-07 17:23:51 +0300

    Asetusruudun viimeisimmät muutokset, ei mitään niin uutta

--- pasianssipeli/src/main/java/com/pasianssipeli/view/AsetusPaneeli.java
+++ pasianssipeli/src/main/java/com/pasianssipeli/view/AsetusPaneeli.java
@@ -8,6 +8,9 @@ public class AsetusPaneeli extends JPanel {
     private JPanel ylamenu;
     private JPanel asetukset;
 
+    private Font tekstinKoko = new Font("Arial", Font.BOLD, 18);
+    private Dimension nappienKoko = new Dimension(140, 30);
+
 
     public AsetusPaneeli(MainPanel mainPanel) {
         // Tykkäsin borderlayoutista, niin käytän sitä komponenttien sijoitteluun.
@@ -42,35 +45,40 @@ public class AsetusPaneeli extends JPanel {
         JPanel asetukset = new JPanel();
         asetukset.setLayout(new GridBagLayout());
 
-        GridBagConstraints asetustenPaikat = new GridBagConstraints();
-        asetustenPaikat.anchor = GridBagConstraints.NORTHWEST; // Komponentit vasemmalle ylös
-        asetustenPaikat.fill = GridBagConstraints.NONE;
-        asetustenPaikat.insets = new Insets(20, 10, 0, 20); // Marginaalit: top, left,bottom, right 
-        asetustenPaikat.weightx = asetustenPaikat.weighty = 0.1; // TODO: Säädä vastamaan muita weightejä
+        GridBagConstraints asetusNappienPaikat = new GridBagConstraints();
+        asetusNappienPaikat.anchor = GridBagConstraints.NORTHWEST; // Komponentit vasemmalle ylös
+        asetusNappienPaikat.fill = GridBagConstraints.HORIZONTAL;
+        asetusNappienPaikat.insets = new Insets(20, 0, 20, 0); // Marginaalit: top, left,bottom, right 
+        asetusNappienPaikat.weightx = asetusNappienPaikat.weighty = 0.1; // TODO: Säädä vastamaan muita weightejä
+
+        // TODO: AAARRRRRRRGGHHH en ymmärrä mikä tossa on että on tommonen luikaus väli ensimmäisen rivin ja tokan rivin välillä
+        GridBagConstraints asetustekstienPaikat = new GridBagConstraints();
+        asetustekstienPaikat.anchor = GridBagConstraints.NORTHWEST; // Komponentit vasemmalle ylös
+        asetustekstienPaikat.fill = GridBagConstraints.HORIZONTAL;
+        asetustekstienPaikat.insets = new Insets(20, 20, 20, 0); // Marginaalit: top, left,bottom, right 
+        asetustekstienPaikat.weightx = asetustekstienPaikat.weighty = 0.1;
+
+        GridBagConstraints oikeanPaneelinPaikka = new GridBagConstraints();
+        oikeanPaneelinPaikka.fill = GridBagConstraints.BOTH;
+        oikeanPaneelinPaikka.weightx = 0.8;
+        oikeanPaneelinPaikka.weighty = 0.1;
+        oikeanPaneelinPaikka.gridx = 5;
+        oikeanPaneelinPaikka.gridy = 0;
+        oikeanPaneelinPaikka.gridheight = 3; // Säätää komponentin korkeutta
 
         GridBagConstraints nollausNapinPaikka = new GridBagConstraints();
-        nollausNapinPaikka.gridwidth = 1; // Säätää komponentin leveyttä
-        nollausNapinPaikka.gridheight = 1; // Säätää komponentin korkeutta
+        nollausNapinPaikka.insets = new Insets(20, 20, 20, 0);
         nollausNapinPaikka.weightx = 0.2;
         nollausNapinPaikka.weighty = 0.2;
         nollausNapinPaikka.gridx = 0;
         nollausNapinPaikka.gridy = 4;
         nollausNapinPaikka.anchor = GridBagConstraints.SOUTHWEST;
 
-        GridBagConstraints oikeanPaneelinPaikka = new GridBagConstraints();
-        oikeanPaneelinPaikka.fill = GridBagConstraints.BOTH;
-        oikeanPaneelinPaikka.weightx = 0.8;
-        oikeanPaneelinPaikka.weighty = 0.2;
-        oikeanPaneelinPaikka.gridx = 5;
-        oikeanPaneelinPaikka.gridy = 0;
-        oikeanPaneelinPaikka.gridheight = 3; // Säätää komponentin korkeutta
-
         GridBagConstraints alemmanPaneelinPaikka = new GridBagConstraints();
         alemmanPaneelinPaikka.fill = GridBagConstraints.NONE;
         alemmanPaneelinPaikka.weighty = 0.5; 
         alemmanPaneelinPaikka.gridx = 0;
         alemmanPaneelinPaikka.gridy = 3;
-        alemmanPaneelinPaikka.gridwidth = 6; // Säätää komponentin leveyttä
  
         // Alustetaan napit
         JToggleButton ajastinNappi = new JToggleButton("Päällä");
@@ -78,22 +86,38 @@ public class AsetusPaneeli extends JPanel {
         JToggleButton aanetNappi = new JToggleButton("Päällä");
         JToggleButton musiikkiNappi = new JToggleButton("Päällä");
 
+        ajastinNappi.setPreferredSize(nappienKoko);
+        siirrotNappi.setPreferredSize(nappienKoko);
+        aanetNappi.setPreferredSize(nappienKoko);
+        musiikkiNappi.setPreferredSize(nappienKoko);
+
+        // TODO: Java ei tue pyöristettyjä nappeja = custom JToggleButton class
+        ajastinNappi.setBorder(null);
+        siirrotNappi.setBorder(null);
+        aanetNappi.setBorder(null);
+        musiikkiNappi.setBorder(null);
+
         // Alustetaan tekstit
-        JLabel ajastinTeksti = new JLabel("<HTML>Näytä<BR>ajastin</HTML>"); // Näin voi tehdä
-        JLabel siirrotTeksti = new JLabel("<HTML>Näytä<BR>siirrot</HTML>");
+        JLabel ajastinTeksti = new JLabel("Näytä ajastin"); // Näin voi tehdä
+        JLabel siirrotTeksti = new JLabel("Näytä siirrot");
         JLabel aanetTeksti = new JLabel("Äänet");
         JLabel musiikkiTeksti = new JLabel("Musiikki");
 
+        ajastinTeksti.setFont(tekstinKoko);
+        siirrotTeksti.setFont(tekstinKoko);
+        aanetTeksti.setFont(tekstinKoko);
+        musiikkiTeksti.setFont(tekstinKoko);
+
 
         // Add labels and toggle buttons to the grid, alternating between label and button
-        addComponent(asetukset, ajastinTeksti, asetustenPaikat,  0, 0); // Rivi 1
-        addComponent(asetukset, ajastinNappi, asetustenPaikat, 1, 0); // Rivi 1
-        addComponent(asetukset, siirrotTeksti, asetustenPaikat, 0, 1); // Rivi 2
-        addComponent(asetukset, siirrotNappi, asetustenPaikat, 1, 1); // Rivi 2
-        addComponent(asetukset, aanetTeksti, asetustenPaikat, 2, 0); // Rivi 1, toinen erä
-        addComponent(asetukset, aanetNappi, asetustenPaikat, 3, 0); // Rivi 1, toinen erä
-        addComponent(asetukset, musiikkiTeksti, asetustenPaikat, 2 , 1); // Rivi 2, toinen erä 
-        addComponent(asetukset, musiikkiNappi, asetustenPaikat, 3, 1); // Rivi 2, toinen erä
+        addComponent(asetukset, ajastinTeksti, asetustekstienPaikat,  0, 0); // Rivi 1
+        addComponent(asetukset, ajastinNappi, asetusNappienPaikat, 1, 0); // Rivi 1
+        addComponent(asetukset, siirrotTeksti, asetustekstienPaikat, 0, 1); // Rivi 2
+        addComponent(asetukset, siirrotNappi, asetusNappienPaikat, 1, 1); // Rivi 2
+        addComponent(asetukset, aanetTeksti, asetustekstienPaikat, 2, 0); // Rivi 1, toinen erä
+        addComponent(asetukset, aanetNappi, asetusNappienPaikat, 3, 0); // Rivi 1, toinen erä
+        addComponent(asetukset, musiikkiTeksti, asetustekstienPaikat, 2 , 1); // Rivi 2, toinen erä 
+        addComponent(asetukset, musiikkiNappi, asetusNappienPaikat, 3, 1); // Rivi 2, toinen erä
 
         // TODO: Jos osaat tehdä paremmin, niin tee pliis :D
         // Näkymätön filleri oikealle
@@ -109,6 +133,7 @@ public class AsetusPaneeli extends JPanel {
         asetukset.add(bottomFiller, alemmanPaneelinPaikka);
 
         JButton nollausNappi = new JButton("Nollaa asetukset");
+        nollausNappi.setPreferredSize(new Dimension(250, 50));
         asetukset.add(nollausNappi, nollausNapinPaikka);
 
         return asetukset;
